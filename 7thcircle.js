85a3884bb201d00276434440cdf374c83396387d
Gerard Paapu <sharkbrainguy@gmail.com>
2011-05-29 12:48:41 +1200

    blah to the blah

--- 7thcircle.js
+++ 7thcircle.js
@@ -18,42 +18,45 @@ remove = function (object, array) {
     array.splice(array.indexOf(object), 1);
 };
 
-GameEntity = function (position, bounding_box, drawing_box) {
+GameEntity = function (world, position, bounding_box, drawing_box) {
     this.position = position || this.position;
     this.bounding_box = bounding_box || this.bounding_box;
     this.drawing_box = drawing_box || this.drawing_box;
 
-    GameEntity.instances.push( this );
+    if (world) {
+        this.world = world;
+        this.world.entitites.push( this );
+    }
 };
 GameEntity.instances = [];
 GameEntity.prototype = { 
-    kill: function () {
-        remove(this, GameEntity.instances);
+    kill: function (world) {
+        remove(this, world.entities);
     }, 
 
-    update: function () {
-
+    update: function (world) {
+        //
     }
 };
 
-Bullet = function (position) {
-    GameEntity.call(this, position); 
-    Bullet.instances.push( this );  
+Bullet = function (world, position) {
+    GameEntity.call(this, world, position); 
+    world.bullets.push(this);  
 };
-Bullet.instances = [];
 Bullet.prototype = Object.create( GameEntity );
-Bullet.prototype.kill = function () {
-    remove(this, Bullet.instances);
+Bullet.prototype.kill = function (world) {
+    remove(this, world.bullets);
     GameEntity.prototype.kill.call(this);
 };
-Bullet.prototype.update = function () {
+Bullet.prototype.update = function (world) {
     this.position = this.position.plus( this.velocity );
-    if (! screen_area.overlaps( this.drawing_box )) {
+
+    if (! world.screen_area.overlaps( this.drawing_box )) {
         this.kill();
     }
 };
 Bullet.prototype.draw = function () {
-
+    
 };
 
 Baddy = function () {
